f016fb2266cac86db9e90b5363f08b5e3dcc7fce
kartikk23 <kartikkhobragade88@gmail.com>
2025-04-28 22:36:10 +0530

    updated startup screen ui version2

--- app/src/main/java/com/agile/officepool/screens/StartupScreen.kt
+++ app/src/main/java/com/agile/officepool/screens/StartupScreen.kt
@@ -2,24 +2,34 @@ package com.agile.officepool.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -32,6 +42,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -51,22 +62,48 @@ fun StartupScreen(navController: NavController) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
-            .padding(16.dp),
+            .verticalScroll(rememberScrollState())
+            .padding(5.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "OfficePool",
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold,
-            style = MaterialTheme.typography.headlineMedium.copy(
+
+        // Top Row with OfficePool Text and Profile Icon
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "OfficePool",
+                fontSize = 25.sp,
                 fontWeight = FontWeight.Bold,
-                color = Color.White
-            ),
-            modifier = Modifier.align(Alignment.Start)
-                .padding(bottom = 15.dp)
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            )
 
 
-        )
+            Box(
+                modifier = Modifier
+                    .width(60.dp)
+                    .clip(RoundedCornerShape(50))
+
+                    .clickable { navController.navigate("profile") }
+                    .padding(10.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+
+
+        }
 
         // Search Bar
         var searchText by remember { mutableStateOf("") }
@@ -96,7 +133,7 @@ fun StartupScreen(navController: NavController) {
                 .padding(horizontal = 16.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            FeatureCard("Travel Together", Color(0xFF2196F3), R.drawable.card_img,modifier = Modifier.weight(1f))
+            FeatureCard("Travel Together", Color(0xFF2196F3), R.drawable.tt1,modifier = Modifier.width(100.dp))
             FeatureCard("Connect", Color(0xFF4CAF50), R.drawable.handshake,modifier = Modifier.weight(1f))
             FeatureCard("Grow", Color(0xFFFFC107), R.drawable.grow,modifier = Modifier.weight(1f))
         }
@@ -131,8 +168,60 @@ fun StartupScreen(navController: NavController) {
 
         // Corporate Booking Card
         CorporateRideCard(R.drawable.card_img)
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Nearest Business Zones",
+            fontSize = 20.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        // Horizontal Scrollable Cards
+        LazyRow(
+            contentPadding = PaddingValues(horizontal = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(businessZones) { zone ->
+                BusinessZoneCard(zone.toString())
+            }
+        }
+
         }
     }
+
+@Composable
+fun BusinessZoneCard(zoneName: String) {
+    Card(
+        modifier = Modifier
+            .width(200.dp)
+            .height(120.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFD1C4E9))
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = zoneName,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF311B92)
+            )
+        }
+    }
+}
+
+// Sample Data
+val businessZones = listOf(
+    "Tech Park Zone",
+    "Industrial Business Hub",
+    "Downtown Business Center"
+)
+
+
+
 
 @Composable
 fun FeatureCard(
@@ -144,7 +233,8 @@ fun FeatureCard(
     Card(
         shape = RoundedCornerShape(16.dp),
         modifier = modifier
-            .height(150.dp),
+            .height(150.dp)
+            .width(300.dp),
 
         colors = CardDefaults.cardColors(containerColor = bgColor)
     ) {
@@ -164,7 +254,7 @@ fun FeatureCard(
             )
             Box(
                 modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
+                contentAlignment = Alignment.BottomCenter
             ) {
                 Text(
                     text = title,
@@ -271,6 +361,7 @@ fun CorporateRideCard(imageResId: Int) {
             }
         }
     }
+
 
 
 
